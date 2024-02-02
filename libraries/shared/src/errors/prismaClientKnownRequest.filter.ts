bb659dd069a513b16223f650f83a27599865333a
Zero <1203970284@qq.com>
2024-02-02 20:11:54 +0800

    feat: add 1059 error

--- libraries/shared/src/errors/prismaClientKnownRequest.filter.ts
+++ libraries/shared/src/errors/prismaClientKnownRequest.filter.ts
@@ -2,10 +2,15 @@ import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { Response } from "express";
 import { CommonLogger } from "../modules/logger/logger.service";
+import { I18nService } from "nestjs-i18n";
+import { I18nTranslations } from "cc.naily.six.generated";
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class CommonPrismaClientKnownRequestErrorFilter implements ExceptionFilter {
-  constructor(private readonly commonLogger: CommonLogger) {
+  constructor(
+    private readonly commonLogger: CommonLogger,
+    private readonly i18nService: I18nService<I18nTranslations>,
+  ) {
     commonLogger.setContext(CommonPrismaClientKnownRequestErrorFilter.name);
   }
 
@@ -16,8 +21,8 @@ export class CommonPrismaClientKnownRequestErrorFilter implements ExceptionFilte
     this.commonLogger.error(`Prisma 数据库请求错误: ${error.message}`);
     response.status(500).json({
       statusCode: 500,
-      code: 1016,
-      message: error.message,
+      code: 1059,
+      message: `${this.i18nService.t("global.errorCode.1059")}: ${error.message}`,
       data: JSON.parse(JSON.stringify(error)),
     });
   }
