1ac15d96e614beb752e0452441ef32509e0455d0
Zero <1203970284@qq.com>
2024-02-16 01:24:47 +0800

    fix(lightning): app store

--- apps/lightning/src/modules/broswer/broswer.module.ts
+++ apps/lightning/src/modules/broswer/broswer.module.ts
@@ -1,20 +1,12 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { NailyContext } from "cc.naily.six.shared";
 import { BrowserMarkController } from "./controllers/mark.controller";
 import { BrowserMarkLockService, BrowserMarkVersionService } from "./providers/mark.service";
 import { BroswerTrackController } from "./controllers/track.controller";
 import { BroswerTrackService } from "./providers/track.service";
-import { DisabledMiddleware } from "./middlewares/disabled.middleware";
 
 @Module({
   controllers: [BrowserMarkController, BroswerTrackController],
   providers: [BrowserMarkLockService, BrowserMarkVersionService, BroswerTrackService],
 })
-export class BroswerModule extends NailyContext implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // prettier-ignore
-    consumer.apply(DisabledMiddleware)
-      .exclude("/broswer/mark/v2", "/broswer/mark/all/v2")
-      .forRoutes(BrowserMarkController);
-  }
-}
+export class BroswerModule extends NailyContext {}
