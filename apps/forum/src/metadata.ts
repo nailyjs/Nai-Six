ae9192e20c4a5d64ac8424a30ca1655d92ee7cd9
Zero <1203970284@qq.com>
2024-02-17 08:59:18 +0800

    chore(forum & shop): add forum app & fix shop build

---
+++ apps/forum/src/metadata.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+export default async () => {
+    const t = {};
+    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: Number } } }]] } };
+};
