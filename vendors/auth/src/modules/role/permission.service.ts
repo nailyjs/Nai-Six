bbca695215d116e04bbad4927002d41860ab765d
Zero <1203970284@qq.com>
2024-02-22 12:13:08 +0800

    fix: role service forward

--- vendors/auth/src/modules/role/permission.service.ts
+++ vendors/auth/src/modules/role/permission.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, forwardRef } from "@nestjs/common";
 import { MustPermission, NotPermission } from "../../constants";
-import { CommonRoleService } from ".";
+import { CommonRoleService } from "./role.service";
 
 @Injectable()
 export class CommonPermissionService {
