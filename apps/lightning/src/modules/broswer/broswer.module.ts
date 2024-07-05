b3c035744f2bbf4ed19befd9ab1c8259e162329d
Naily <zero@naily.cc>
2024-07-05 13:51:45 +0800

    feat(LockService): lockService impl

--- apps/lightning/src/modules/broswer/broswer.module.ts
+++ apps/lightning/src/modules/broswer/broswer.module.ts
@@ -4,9 +4,10 @@ import { BrowserMarkController } from "./controllers/mark.controller";
 import { BrowserMarkLockService, BrowserMarkVersionService } from "./providers/mark.service";
 import { BroswerTrackController } from "./controllers/track.controller";
 import { BroswerTrackService } from "./providers/track.service";
+import { BrowserMarkRedlockService } from "./providers/markRedlock.service";
 
 @Module({
   controllers: [BrowserMarkController, BroswerTrackController],
-  providers: [BrowserMarkLockService, BrowserMarkVersionService, BroswerTrackService],
+  providers: [BrowserMarkLockService, BrowserMarkVersionService, BroswerTrackService, BrowserMarkRedlockService],
 })
 export class BroswerModule extends NailyContext {}
---
+++ apps/lightning/src/modules/broswer/lock.interface.ts
@@ -0,0 +1,24 @@
+export interface LockService {
+  /**
+   * 检查是否可以查询
+   *
+   * @param {string} userID
+   * @return {*}  {boolean}
+   * @memberof LockService
+   */
+  canFind(userID: string): boolean;
+  /**
+   * 添加`更新中`状态
+   *
+   * @param {string} userID
+   * @memberof LockService
+   */
+  addUpdating(userID: string): void;
+  /**
+   * 移除`更新中`状态
+   *
+   * @param {string} userID
+   * @memberof LockService
+   */
+  removeUpdating(userID: string): void;
+}
